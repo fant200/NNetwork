d6650d6168fb27b543311e8895c27db95f0590b4
fant200 <demodema002@o2.pl>
2016-06-02 22:36:54 +0200

    Initialization of neural layer, cleanup after winforms

--- WindowsFormsApplication2/NeuralLayer.cs
+++ WindowsFormsApplication2/NeuralLayer.cs
@@ -8,7 +8,24 @@ namespace WindowsFormsApplication2
 {
     class NeuralLayer
     {
-        Matrix Weights;
+        Matrix weights,biases;
+        NeuralLayer prevLayer;
+        Matrix errors,deltas;
+        Matrix layerInput, layerOutput;
+        int Size => weights.Rows;
+        int Inputs => weights.Columns;
+
+        NeuralLayer(int layerSize, int numOfInputs)
+        {
+            weights = new Matrix(layerSize, numOfInputs);
+            errors = new Matrix(layerSize, 1);
+            deltas = new Matrix(layerSize, 1);
+            layerInput = new Matrix(layerSize, 1);
+        }
+        NeuralLayer(int layerSize, NeuralLayer _prevLayer): this(layerSize, _prevLayer.Size)
+        {
+
+        }
 
     }
 }
