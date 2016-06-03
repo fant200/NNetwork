1f5ffe13b5addee6b0ea85b14ff185c142ee337d
fant200 <demodema002@o2.pl>
2016-06-03 12:19:23 +0200

    Create unit test

---
+++ NetTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Nets;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace NetTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        static Random rng = new Random();
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void MulMatrixWithWrongDim()
+        {
+            Matrix a = new Matrix(3, 3);
+            Matrix b = new Matrix(4, 4);
+            a = a * b;
+        }
+    }
+}
